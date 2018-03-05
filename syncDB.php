46f878e7c49056024d1db82fa2b61faa75389c84
nxn_cho <khuuen@gmail.com>
2018-03-05 13:03:34 +0100

    followersHandles in a clean array in syncDB.php

--- syncDB.php
+++ syncDB.php
@@ -9,19 +9,25 @@ $ACCESS_TOKEN = "179333574-ZbVWfe5F3TxMCXdpj6cOwZ3ZhKb3FxdHdu1AoDd9";
 $ACCESS_SECRET = "xq0Q2h5bbOtyteG4d89LraUukhYThzMQ0v7qeUM9QGgs7";
 
 
-//$connection = new TwitterOAuth($CONSUMER_KEY, $CONSUMER_SECRET, $ACCESS_TOKEN, $ACCESS_SECRET);
 
 
-    $usernamePicked = file_get_contents("usernameToCheck.json");
-    $usernamePicked = json_decode($usernamePicked, true);
-    var_dump($usernamePicked);
-    // $followersList = $connection->get('followers/list',array('screen_name' => $usernamePicked )); //'cursor' => $cursor_value
-    // $IdsAndUsernames = array();
-    //
-    // $followersList = get_object_vars($followersList);
-    // foreach ($followersList['users'] as $key => $value)
-    // {
-    //   $user = get_object_vars($value);
-    //   print_r($user['screen_name'] . '<br>');
-    // }
+
+    $usernameFromConfig = file_get_contents("usernameToCheck.json");
+    $usernameFromConfig = json_decode($usernameFromConfig, true);
+    $usernameFromConfig = $usernameFromConfig['username'];
+
+    $connection = new TwitterOAuth($CONSUMER_KEY, $CONSUMER_SECRET, $ACCESS_TOKEN, $ACCESS_SECRET);
+    $followersList = $connection->get('followers/list',array('screen_name' => $usernameFromConfig )); //'cursor' => $cursor_value
+
+
+     $followersList = get_object_vars($followersList);
+     $followersHandles = array();
+     foreach ($followersList['users'] as $key => $value)
+     {
+       $user = get_object_vars($value);
+       $followersHandles[] = $user['screen_name'];
+       //print_r($user['screen_name'] . '<br>');
+     }
+
+     var_dump($followersHandles);
  ?>
